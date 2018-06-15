72d7ff31ce93c66653275280666d5d27dc7ed3ec
Amadeusz Wieczorek <git@amadeusw.com>
2018-06-14 21:14:31 -0700

    init

---
+++ Surround.VS/CommandHandlers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surround.VS
+{
+    class CommandHandlers // implement ICommandHandlers for TypedChar
+    {
+        // Check if there is a selection
+        // Check if selection is on the word boundary
+        // If there is, add a matching character
+    }
+}
